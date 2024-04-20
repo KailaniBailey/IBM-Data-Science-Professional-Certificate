c1a2eaf75dcde09ac0da2bd2b50ea54645b71d9a
Kailani Bri Bailey <158431578+KailaniBailey@users.noreply.github.com>
2024-04-20 17:56:51 -0500

    Update strings.py

--- 04. Python for Data Science, AI & Development/Week 1: Python Basics/strings.py
+++ 04. Python for Data Science, AI & Development/Week 1: Python Basics/strings.py
@@ -1,1 +1,60 @@
+# Quotation marks for defining string
+"Michael Jackson"
+# Can use single quotes
+'Michael Jackson'
+
+# Combination of spaces and strings
+'1, 2, 3, 4, 5 "
+
+# Combination of special characters
+"!@#$%^& *()_+"
+
+# Using a print statement
+print('Hello!')
+
+# Assign string to a variable
+name = "Michael Jackson"
+name
+
+# Indexing starts at 0; the first index is located at 0
+# Print first element in the string
+print(name[0])
+# Result is M
+
+# Access index 6
+print(name[6])
+# Result is l
+
+# Access index 13
+print(name[13])
+# Result is o
+
+# Negative indexing starts at -1, not 0
+print(name[-1])
+# Result is n
+
+# Obtain first element by using index -15
+print(name[-15])
+# Result is M
+
+# Can find the number of characters in a string by using 'len'
+len("Michael Jackson")
+# Result is 15
+
+# Can obtain multiple characters in a string by SLICING; FIRST NUMBER is start point, SECOND NUMBER is one index before the given point; 
+# Using slice on the variable 'name' with only index  0 to 3
+name[0:4]
+# Result is "Mich"
+name[8:11]
+# Result is "Jack"
+
+# Can skip every other index by using STRIDE; For example 2 selects every 2 instances
+Name[::2]
+# Result is "McalJcsn"
+
+# Can import SLICING with the STRIDE
+
+
+
+
 
