3ca6b6bf8d5b1f3a11c53e84185536743342039f
Kailani Bri Bailey <158431578+KailaniBailey@users.noreply.github.com>
2024-04-20 15:30:01 -0500

    Update WriteFirstPythonCode.py

--- 04. Python for Data Science, AI & Development/WriteFirstPythonCode.py
+++ 04. Python for Data Science, AI & Development/WriteFirstPythonCode.py
@@ -17,14 +17,46 @@ frint('Hello, Python!')
 # Python will stop executing entire program when it encounters an error... script following the error will not get printed.
 print('Hello, Python!)
 
-# Integer
+# Integer (integers can be negative or positive, includes zero)
 11
 
-# Float
+# Float (decimal points, aka real numbers)
 2.14
+# System settings about float type
+sys.float_info
 
 # String
 "Hello, Python 101"
 
+# Type of 12 is integer
+type(12)
 
+# Type of 2.14 is float
+type(2.14)
 
+# Type of "Hello, Python 101!" is string
+type('Hello, Python 101!')
+
+# Typecasting: changing from one type to another
+float(2) # Converting integers to floats
+# Result is 2.0
+
+# Converting 2 to a float then checking it's type
+type(float(2)) # Type will be a float
+
+# Changing 1.1 to an integer will lose the additional information of .1
+int(1.1) # Result will be 1 (with the lost information of .1)
+
+# Convert string into an integer
+int('1') # Result is 1
+
+# Convert string '1.2' into a float
+float('1.2') # Result is 1.2
+
+# Strings can be represented using single quotes or double quotes but not both at the same time
+
+# Convert integer into a string
+str(1) # Result is '1'
+
+# Convert float into a string
+str(1.2) # Result is '1.2'
