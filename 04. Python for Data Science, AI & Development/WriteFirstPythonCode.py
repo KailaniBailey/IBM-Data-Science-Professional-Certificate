e79400b6efdc6a84c1de203dd84c4ab8ecc4871a
Kailani Bri Bailey <158431578+KailaniBailey@users.noreply.github.com>
2024-04-20 15:17:32 -0500

    Update WriteFirstPythonCode.py

--- 04. Python for Data Science, AI & Development/WriteFirstPythonCode.py
+++ 04. Python for Data Science, AI & Development/WriteFirstPythonCode.py
@@ -1,1 +1,30 @@
+# Trying first python output for Course 4
+print('Hello, Python!')
+
+# Check the python version
+import sys
+print(sys.version)
+
+# Practice writing comments
+print('Hello, Python!') # This prints a string
+
+# print('Hi')... this line of code has been 'commented out'
+
+# Prints string as error message due to the typo
+frint('Hello, Python!')
+
+# Prints syntax error due to open string... interpreted language: python interprets script line by line as it's executed.
+# Python will stop executing entire program when it encounters an error... script following the error will not get printed.
+print('Hello, Python!)
+
+# Integer
+11
+
+# Float
+2.14
+
+# String
+"Hello, Python 101"
+
+
 
